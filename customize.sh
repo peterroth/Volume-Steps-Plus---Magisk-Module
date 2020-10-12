fd9ed9d9d761fb2e47a99f1b04a046ea4f077fa1
Peter Roth <peter@roth.su>
2020-10-12 11:37:59 +0200

    Set correct permissions

--- customize.sh
+++ customize.sh
@@ -13,17 +13,4 @@
 # Permissions
 ##########################################################################################
 
-set_permissions() {
-  # Only some special files require specific permissions
-  # The default permissions should be good enough for most cases
-
-  # Here are some examples for the set_perm functions:
-
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
-  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
-  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
-  set_perm_recursive $MODPATH/system/*bin   0 2000 0755 0755
-}
+set_perm_recursive $MODPATH/system/xbin 0 0 0755 0755
